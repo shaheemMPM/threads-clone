b7007ff810b62658b7f4b65215f183fa46e3066e
shaheemMPM <shaheem.mpm@gmail.com>
2023-09-01 16:44:25 +0530

    Adds thread contents to UI

--- components/Thread/index.tsx
+++ components/Thread/index.tsx
@@ -1,8 +1,11 @@
 import { View } from "react-native";
+import { Image } from "expo-image";
 import { Thread } from "../../types";
 import PostHeading from "./PostHeading";
 import BottomIcons from "./BottomIcons";
 import PostFooter from "./PostFooter";
+import { Text } from "../Themed";
+import { blurHash } from "../../constants/general";
 
 export default function ThreadItem(thread: Thread): JSX.Element {
 	return (
@@ -13,6 +16,16 @@ export default function ThreadItem(thread: Thread): JSX.Element {
 					verified={thread.author.verified}
 					createdAt={thread.createdAt}
 				/>
+				<Text>{thread.content}</Text>
+				{thread.image && (
+					<Image
+						source={thread.image}
+						style={{ width: "100%", minHeight: 300, borderRadius: 10 }}
+						placeholder={blurHash}
+						contentFit="cover"
+						transition={500}
+					/>
+				)}
 				<BottomIcons />
 				<PostFooter replies={thread.repliesCount} likes={thread.likesCount} />
 			</View>

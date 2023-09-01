63aac48909119ef5bba6cb9ee2bdf35b620b67ea
shaheemMPM <shaheem.mpm@gmail.com>
2023-09-01 16:39:30 +0530

    Adds thread bottom icons component to UI

--- components/Thread/index.tsx
+++ components/Thread/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 import { Thread } from "../../types";
 import PostHeading from "./PostHeading";
+import BottomIcons from "./BottomIcons";
 import PostFooter from "./PostFooter";
 
 export default function ThreadItem(thread: Thread): JSX.Element {
@@ -12,6 +13,7 @@ export default function ThreadItem(thread: Thread): JSX.Element {
 					verified={thread.author.verified}
 					createdAt={thread.createdAt}
 				/>
+				<BottomIcons />
 				<PostFooter replies={thread.repliesCount} likes={thread.likesCount} />
 			</View>
 		</View>
